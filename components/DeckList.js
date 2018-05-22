cad9239939821f3aba84fa86daf7bfcce6e3f4dc
Geocarlos Alves <geowildcat@gmail.com>
2018-05-22 15:40:17 -0400

    Initial component files

---
+++ components/DeckList.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class DeckList extends Component{
+  render(){
+    return (
+      <View>
+        <Text>DECK LIST</Text>
+      </View>
+    )
+  }
+}
